6b0885aa97bddc26422a7fa79c5156aae0b97eff
Chris Coffey <ccoffey@inmotionnow.com>
2019-03-11 01:49:03 -0400

    Adding passport and API sessions.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,13 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import bluebird from 'bluebird'
 import bodyParser from 'body-parser'
+import session from 'express-session'
+import uuid from 'uuid/v4'
+import sessionFileStore from 'session-file-store'
+
 
 dotenv.config()
+const FileStore = sessionFileStore(session)
 
 const app = express()
 
@@ -15,10 +20,37 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .catch(error => console.log('Mongo connection error. ' + error))
 
 app.set('port', process.env.API_PORT)
+app.use(session({
+  genid: (req) => {
+    console.log('genid callback')
+    console.log(req.sessionID)
+    return uuid()
+  },
+  store: new FileStore(),
+  secret: 'something',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send({ message: 'hello world!'}))
+app.get('/', (req, res) => {
+  console.log('inside GET')
+  console.log(req.sessionID)
+  res.send({ message: 'hello world!'})
+})
+
+app.get('/login', (req, res) => {
+  console.log('inside GET /login')
+  console.log(req.sessionID)
+  res.send('Login page\n')
+})
+
+app.post('/login', (req, res) => {
+  console.log('inside POST /login')
+  console.log(req.body)
+  res.send('POST to login page\n')
+})
 
 app.listen(process.env.API_PORT,  () => {
   console.log(`Listening on port ${process.env.API_PORT}`)
