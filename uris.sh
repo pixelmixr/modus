9aeaae1060c1b2291f74e5e26a46cc116bcf9b64
Kevin Coffey <kevdougful@gmail.com>
2019-03-17 10:41:20 -0500

    Add Server to Compose File
    Haven't figured out hot reload yet, probably need to connect in the src directory with a bind volume.

--- uris.sh
+++ uris.sh
@@ -5,9 +5,7 @@
 source .env
 rm server/.env
 
-MONGODB_URI="MONGODB_URI=mongodb://$MODUS_USERNAME:$MODUS_PASSWORD@localhost:27017/$MODUS_DATABASE"
-SESSIONDB_URI="SESSIONDB_URI=mongodb://$MODUS_SESSION_USERNAME:$MODUS_SESSION_PASSWORD@localhost:27017/$MODUS_SESSION_DATABASE"
-echo $MONGODB_URI >> server/.env
-echo $SESSIONDB_URI >> server/.env
+echo "MONGODB_URI=$MONGODB_URI" >> server/.env
+echo "SESSIONDB_URI=$SESSIONDB_URI" >> server/.env
 echo "API_PORT=$API_PORT" >> server/.env
 echo "SESSION_SECRET=$SESSION_SECRET" >> server/.env
